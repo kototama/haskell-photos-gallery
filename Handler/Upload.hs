82e2c73f5ac9e87364dae418dfbf69889d4899f4
Kototama <kototamo@gmail.com>
2013-08-31 15:48:59 +0200

    integration of dropzone

---
+++ Handler/Upload.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+module Handler.Upload where
+
+import Import
+import qualified Data.Text.IO as T
+import Yesod.Core.Types
+
+getUploadR :: Handler Html
+getUploadR = do
+    (formWidget, formEnctype) <- generateFormPost uploadForm
+    let submission = Nothing :: Maybe (FileInfo, Text)
+        handlerName = "getUploadR" :: Text
+    defaultLayout $ do
+      addStylesheet $ StaticR css_dropzone_css
+      addScript $ StaticR js_dropzone_js
+      aDomId <- newIdent
+      setTitle "Welcome To Yesod!"
+      $(widgetFile "uploadpage")
+
+postUploadR :: Handler Html
+postUploadR = do
+     fileInfo <- runInputGet $ ireq fileField "Select file"
+     let FileInfo fName fContentType fSourceRaw fMove = fileInfo
+
+     defaultLayout $ do
+       addScript $ StaticR js_dropzone_js
+       aDomId <- newIdent
+       lift $ putStr "Submission ="
+       lift $ T.putStr fName
+       -- lift $ fMove "/tmp/thamer"
+
+       setTitle "Welcome To Yesod!"
+       $(widgetFile "uploadpage")
+
+uploadForm :: Form (FileInfo, Text)
+uploadForm = renderDivs $ (,)
+    <$> fileAFormReq "Choose a file"
+    <*> areq textField "What's on the file?" Nothing
