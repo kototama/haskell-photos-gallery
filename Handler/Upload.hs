e760e23d9f87dde12527a1d203644e96164ba472
Kototama <kototamo@gmail.com>
2013-08-31 16:31:21 +0200

    upload of PNG and JPG files

--- Handler/Upload.hs
+++ Handler/Upload.hs
@@ -2,6 +2,7 @@
 module Handler.Upload where
 
 import Import
+import Data.Text (unpack)
 import qualified Data.Text.IO as T
 import Yesod.Core.Types
 
@@ -17,22 +18,27 @@ getUploadR = do
       setTitle "Welcome To Yesod!"
       $(widgetFile "uploadpage")
 
+displayUploadPage :: Handler Html
+displayUploadPage = defaultLayout $ do
+  setTitle "Upload page"
+  $(widgetFile "uploadpage")
+
+copyFileToGallery :: Text -> ([Char] -> IO ()) -> HandlerT App IO Html
+copyFileToGallery fName fMove = do
+     lift $ fMove ("/tmp/upload/" ++ (unpack fName))
+     displayUploadPage
+
 postUploadR :: Handler Html
 postUploadR = do
      -- dropzone generates <input type="file" name="file" />
 
      fileInfo <- runInputPost $ ireq fileField "file"
-     let FileInfo fName fContentType fSourceRaw fMove = fileInfo
+     let FileInfo fName fContentType _ fMove = fileInfo
 
-     defaultLayout $ do
-       addScript $ StaticR js_dropzone_js
-       aDomId <- newIdent
-       lift $ putStr "Submission ="
-       lift $ T.putStr fName
-       lift $ fMove "/tmp/thamer"
-
-       setTitle "Welcome To Yesod!"
-       $(widgetFile "uploadpage")
+     case (unpack fContentType) of
+       "image/png" -> copyFileToGallery fName fMove
+       "image/jpeg" -> copyFileToGallery fName fMove
+       _ -> invalidArgs ["Only PNG, JPEG and Zip files are accepted"]
 
 uploadForm :: Form (FileInfo, Text)
 uploadForm = renderDivs $ (,)
